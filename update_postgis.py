6e0aa83b6105ea62ced17cfc4ec0373593551a1c
serranoycandela <serranoycandela@gmail.com>
2017-03-10 17:25:45 -0600

    grab url and step from args

--- update_postgis.py
+++ update_postgis.py
@@ -1,22 +1,19 @@
+import argparse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import text
 import time
 
+parser = argparse.ArgumentParser(description='set time step')
+parser.add_argument('--db_url', default='sqlite:///:memory:', help='DB URL, default: sqlite:///:memory:')
+parser.add_argument('--step', type=int, help='time step to set')
+args = parser.parse_args()
 
-
-engine  = create_engine("postgresql+psycopg2://postgres:x@localhost/new")
+engine  = create_engine(args.db_url)
 conn = engine.connect()
 
-
-for i in range (1,30):
-    time.sleep(0.5)
-    esteTimeStep = int(12*i)
-    s = text("select * from infra_h where timestep = %s" % esteTimeStep)
-    for row in conn.execute(s).fetchall():
-        ageb = dict(row)
-        #print ageb['ageb_id']
-        
-        update_infra = text("UPDATE agebs_calles_geo SET infra= :infra WHERE ageb_id= :ageb_id")
-        
-        conn.execute(update_infra, infra = ageb['infra'], ageb_id = ageb['ageb_id'])
+s = text("select ageb_id, infra from infra_h where timestep = %s" % args.step)
+for row in conn.execute(s).fetchall():
+    ageb = dict(row)
+    update_infra = text("UPDATE agebs_calles_geo SET infra= :infra WHERE ageb_id= :ageb_id")
+    conn.execute(update_infra, infra = ageb['infra'], ageb_id = ageb['ageb_id'])
